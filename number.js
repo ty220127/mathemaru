be9cdd603f82cc989dd75bcffbcbbf6caa219e93
ty220127 <98523610+ty220127@users.noreply.github.com>
2023-04-06 23:04:24 +0900

    Update number.js

--- number.js
+++ number.js
@@ -11,18 +11,20 @@ i = 0;
 
 //2:Markov
 i++; markov = i;
+bns[i] = [];
 refs[i] = "https://ty220127.github.io/mathemaru/markov.html";
 ttls[i] = "Markov 連鎖";
 j = 0;
-j++; mpath = j;
+j++; mpath = j; bns[i][j] = "命題：パスの確率";
 j++; y = j;
 j++; x = j;
 
 //1:RW
 i++; rw1 = i;
+bns[i] = [];
 refs[i] = "https://ty220127.github.io/mathemaru/random-walk-z.html";
 ttls[i] = "１次元ランダムウォーク";
 j = 0;
-j++; returnprob = j;
+j++; returnprob = j; bns[i][j] = "命題：再帰確率は $1$";
 j++; y = j;
 j++; x = j;
