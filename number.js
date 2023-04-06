621a82e81cb7ca1455f6f3c51c972af2c2f339f8
ty220127 <98523610+ty220127@users.noreply.github.com>
2023-04-06 18:06:39 +0900

    Create number.js

---
+++ number.js
@@ -0,0 +1,13 @@
+//Numbering-Rule
+function nr(model,box) {
+  document.write("[#" + model + "-" + box + "]")
+}
+
+i = 0;
+
+//1:RW
+i++; rw1 = i;
+j = 0;
+j++; returnprob = j;
+j++; y = j;
+j++; x = j;
