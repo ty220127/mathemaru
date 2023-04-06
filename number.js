2ccfc07d7a4b7c2c23975028083e0c05f1ffdb2f
ty220127 <98523610+ty220127@users.noreply.github.com>
2023-04-06 22:31:37 +0900

    Update number.js

--- number.js
+++ number.js
@@ -4,12 +4,14 @@ function nr(model,box) {
 }
 
 var refs = [];
+var ttls = [];
 
 i = 0;
 
 //1:RW
 i++; rw1 = i;
 refs[i] = "https://ty220127.github.io/mathemaru/random-walk-z.html";
+ttls[i] = "１次元ランダムウォーク";
 j = 0;
 j++; returnprob = j;
 j++; y = j;
