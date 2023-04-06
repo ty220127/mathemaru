7406e19ffb3f860d275f4c69f8d2a10a63e26654
ty220127 <98523610+ty220127@users.noreply.github.com>
2023-04-06 22:09:38 +0900

    Update number.js

--- number.js
+++ number.js
@@ -3,10 +3,13 @@ function nr(model,box) {
   document.write("[#" + model + "-" + box + "]")
 }
 
+var refs = [];
+
 i = 0;
 
 //1:RW
 i++; rw1 = i;
+refs[i] = "https://ty220127.github.io/mathemaru/random-walk-z.html";
 j = 0;
 j++; returnprob = j;
 j++; y = j;
