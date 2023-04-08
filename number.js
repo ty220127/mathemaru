b9005306fa486a5c202cf984b1379d355eaccd9f
ty220127 <98523610+ty220127@users.noreply.github.com>
2023-04-08 13:17:20 +0900

    Update number.js

--- number.js
+++ number.js
@@ -9,21 +9,21 @@ var bns = [];
 
 i = 0;
 
-//3:measurable-space
+//3:measurable_space
 i++; measurable = i;
 bns[i] = [];
-refs[i] = "https://ty220127.github.io/mathemaru/sigma-algebra.html";
+refs[i] = "https://ty220127.github.io/mathemaru/sigma_algebra.html";
 ttls[i] = "可測空間";
 j = 0;
-j++; measurable-power = j; bns[i][j] = "例：冪集合は可測空間を成す";
-j++; measurable-minimum = j; bns[i][j] = "例：最小の $\sigma$ 加法族による可測空間";
-j++; measurable-intersection = j; bns[i][j] = "命題：可測集合の共通部分の可測性";
-j++; measurable-limit = j; bns[i][j] = "命題：上（下）極限集合の可測性";
-j++; measurable-difference = j; bns[i][j] = "命題：可測集合の差の可測性";
-j++; sigma-intersection = j; bns[i][j] = "命題：共通部分による $\sigma$ 加法族";
-j++; sigma-generation = j; bns[i][j] = "例：$\sigma$ 加法族の生成";
-j++; sigma-gen-unique = j; bns[i][j] = "例：$\sigma$ 加法族の生成の一意性";
-j++; sigma-gen-inclusion = j; bns[i][j] = "例：生成される $\sigma$ 加法族の包含";
+j++; measurable_power = j; bns[i][j] = "例：冪集合は可測空間を成す";
+j++; measurable_minimum = j; bns[i][j] = "例：最小の $\sigma$ 加法族による可測空間";
+j++; measurable_intersection = j; bns[i][j] = "命題：可測集合の共通部分の可測性";
+j++; measurable_limit = j; bns[i][j] = "命題：上（下）極限集合の可測性";
+j++; measurable_difference = j; bns[i][j] = "命題：可測集合の差の可測性";
+j++; sigma_intersection = j; bns[i][j] = "命題：共通部分による $\sigma$ 加法族";
+j++; sigma_generation = j; bns[i][j] = "例：$\sigma$ 加法族の生成";
+j++; sigma_gen_unique = j; bns[i][j] = "例：$\sigma$ 加法族の生成の一意性";
+j++; sigma_gen_inclusion = j; bns[i][j] = "例：生成される $\sigma$ 加法族の包含";
 
 //2:Markov
 i++; markov = i;
