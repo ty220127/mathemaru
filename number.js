c1082d683c7011803fcadd872e3517c07293dc2f
ty220127 <98523610+ty220127@users.noreply.github.com>
2023-04-08 14:43:55 +0900

    Update number.js

--- number.js
+++ number.js
@@ -16,14 +16,15 @@ refs[i] = "https://ty220127.github.io/mathemaru/sigma-algebra.html";
 ttls[i] = "可測空間";
 j = 0;
 j++; measurable_power = j; bns[i][j] = "例：冪集合は可測空間を成す";
-j++; measurable_minimum = j; bns[i][j] = "例：最小の $\sigma$ 加法族による可測空間";
+j++; measurable_minimum = j; bns[i][j] = "例：最小のσ加法族による可測空間";
+j++; measurable_fin_add = j; bns[i][j] = "命題：σ加法族の有限加法性";
 j++; measurable_intersection = j; bns[i][j] = "命題：可測集合の共通部分の可測性";
 j++; measurable_limit = j; bns[i][j] = "命題：上（下）極限集合の可測性";
 j++; measurable_difference = j; bns[i][j] = "命題：可測集合の差の可測性";
-j++; sigma_intersection = j; bns[i][j] = "命題：共通部分による $\sigma$ 加法族";
-j++; sigma_generation = j; bns[i][j] = "例：$\sigma$ 加法族の生成";
-j++; sigma_gen_unique = j; bns[i][j] = "例：$\sigma$ 加法族の生成の一意性";
-j++; sigma_gen_inclusion = j; bns[i][j] = "例：生成される $\sigma$ 加法族の包含";
+j++; sigma_intersection = j; bns[i][j] = "命題：共通部分によるσ加法族";
+j++; sigma_generation = j; bns[i][j] = "例：σ加法族の生成";
+j++; sigma_gen_unique = j; bns[i][j] = "例：σ加法族の生成の一意性";
+j++; sigma_gen_inclusion = j; bns[i][j] = "例：生成されるσ加法族の包含";
 
 //2:Markov
 i++; markov = i;
