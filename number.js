fec74d83dc5f7a8aa503deb55b273b5ff481fe52
ty220127 <98523610+ty220127@users.noreply.github.com>
2023-05-17 22:09:21 +0900

    Update number.js

--- number.js
+++ number.js
@@ -7,71 +7,64 @@ var refs = [];
 var ttls = [];
 var bns = [];
 
-i = 0;
-
-//6:numerical-vector-space
-i++; numvec = i;
-bns[i] = [];
-refs[i] = "https://ty220127.github.io/mathemaru/numerical-vector.html";
-ttls[i] = "数ベクトル空間";
-j = 0;
-j++; numvecprop = j; bns[i][j] = "命題：数ベクトルの交換・結合・分配法則";
-j++; invnumvec = j; bns[i][j] = "定義：逆ベクトル，数ベクトルの減法";
-j++; zeronumvec = j; bns[i][j] = "定義：零ベクトル";
-j++; invzeronumvecprop = j; bns[i][j] = "命題：逆・零ベクトルに関する性質";
-
-//3:measurable_space
-i++; measurable = i;
-bns[i] = [];
-refs[i] = "https://ty220127.github.io/mathemaru/sigma-algebra.html";
-ttls[i] = "可測空間";
-j = 0;
-j++; measurable_power = j; bns[i][j] = "例：冪集合は可測空間を成す";
-j++; measurable_minimum = j; bns[i][j] = "例：最小のσ加法族による可測空間";
-j++; measurable_fin_add = j; bns[i][j] = "命題：σ加法族の有限加法性";
-j++; measurable_intersection = j; bns[i][j] = "命題：可測集合の共通部分の可測性";
-j++; measurable_limit = j; bns[i][j] = "命題：上（下）極限集合の可測性";
-j++; measurable_difference = j; bns[i][j] = "命題：可測集合の差の可測性";
-j++; measurable_empty_universal = j; bns[i][j] = "命題：空集合及び全体集合の可測性";
-j++; sigma_by_intersection = j; bns[i][j] = "命題：共通部分によるσ加法族";
-j++; sigma_intersection = j; bns[i][j] = "命題：σ加法族の共通部分";
-j++; sigma_generation = j; bns[i][j] = "例：σ加法族の生成";
-j++; sigma_gen_unique = j; bns[i][j] = "例：σ加法族の生成の一意性";
-j++; sigma_gen_inclusion = j; bns[i][j] = "例：生成されるσ加法族の包含";
-
-//2:Markov
-i++; markov = i;
-bns[i] = [];
-refs[i] = "https://ty220127.github.io/mathemaru/markov.html";
-ttls[i] = "Markov 連鎖";
-j = 0;
-j++; mpath = j; bns[i][j] = "命題：パスの確率";
-j++; mst = j; bns[i][j] = "定義：定常分布";
-j++; x = j;
-
 //1:RW
-i++; rw1 = i;
-bns[i] = [];
-refs[i] = "https://ty220127.github.io/mathemaru/random-walk-z.html";
-ttls[i] = "１次元ランダムウォーク";
+bns[1] = [];
+refs[1] = "https://ty220127.github.io/mathemaru/random-walk-z.html";
+ttls[1] = "１次元ランダムウォーク";
 j = 0;
-j++; returnprob = j; bns[i][j] = "命題：再帰確率は $1$";
+j++; returnprob = j; bns[1][j] = "命題：再帰確率は $1$";
 j++; y = j;
 j++; x = j;
 
-//5:norm-space
-i++; norm = i;
-bns[i] = [];
-refs[i] = "https://ty220127.github.io/mathemaru/norm-space.html";
-ttls[i] = "ノルム空間";
+//2:Markov
+bns[2] = [];
+refs[2] = "https://ty220127.github.io/mathemaru/markov.html";
+ttls[2] = "Markov 連鎖";
 j = 0;
-j++; normisdist = j; bns[i][j] = "ノルム空間は距離空間";
+j++; mpath = j; bns[2][j] = "命題：パスの確率";
+j++; mst = j; bns[2][j] = "定義：定常分布";
+j++; x = j;
+
+//3:measurable_space
+bns[3] = [];
+refs[3] = "https://ty220127.github.io/mathemaru/sigma-algebra.html";
+ttls[3] = "可測空間";
+j = 0;
+j++; measurable_power = j; bns[3][j] = "例：冪集合は可測空間を成す";
+j++; measurable_minimum = j; bns[3][j] = "例：最小のσ加法族による可測空間";
+j++; measurable_fin_add = j; bns[3][j] = "命題：σ加法族の有限加法性";
+j++; measurable_intersection = j; bns[3][j] = "命題：可測集合の共通部分の可測性";
+j++; measurable_limit = j; bns[3][j] = "命題：上（下）極限集合の可測性";
+j++; measurable_difference = j; bns[3][j] = "命題：可測集合の差の可測性";
+j++; measurable_empty_universal = j; bns[3][j] = "命題：空集合及び全体集合の可測性";
+j++; sigma_by_intersection = j; bns[3][j] = "命題：共通部分によるσ加法族";
+j++; sigma_intersection = j; bns[3][j] = "命題：σ加法族の共通部分";
+j++; sigma_generation = j; bns[3][j] = "例：σ加法族の生成";
+j++; sigma_gen_unique = j; bns[3][j] = "例：σ加法族の生成の一意性";
+j++; sigma_gen_inclusion = j; bns[3][j] = "例：生成されるσ加法族の包含";
 
 //4:inner-product-space
-i++; inprod = i;
-bns[i] = [];
-refs[i] = "https://ty220127.github.io/mathemaru/inner-product-space.html";
-ttls[i] = "内積空間";
+bns[4] = [];
+refs[4] = "https://ty220127.github.io/mathemaru/inner-product-space.html";
+ttls[4] = "内積空間";
 j = 0;
-j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
-j++; inprodisnorm = j; bns[i][j] = "内積空間はノルム空間";
+j++; antilinearity = j; bns[4][j] = "第二成分の反線形性";
+j++; inprodisnorm = j; bns[4][j] = "内積空間はノルム空間";
+
+//5:norm-space
+bns[5] = [];
+refs[5] = "https://ty220127.github.io/mathemaru/norm-space.html";
+ttls[5] = "ノルム空間";
+j = 0;
+j++; normisdist = j; bns[5][j] = "ノルム空間は距離空間";
+
+//6:numerical-vector-space
+bns[6] = [];
+refs[6] = "https://ty220127.github.io/mathemaru/numerical-vector.html";
+ttls[6] = "数ベクトル空間";
+j = 0;
+j++; numvecprop = j; bns[6][j] = "命題：数ベクトルの交換・結合・分配法則";
+j++; invnumvec = j; bns[6][j] = "定義：逆ベクトル，数ベクトルの減法";
+j++; zeronumvec = j; bns[6][j] = "定義：零ベクトル";
+j++; invzeronumvecprop = j; bns[6][j] = "命題：逆・零ベクトルに関する性質";
+
